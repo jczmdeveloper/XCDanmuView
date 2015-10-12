01ea334540dd33d4247c41c4070328073770470a
Jamy Cai <jczmdeveloper@gmail.com>
2015-10-12 17:56:20 +0800

    commit danmu view project
    commit danmu view project

---
+++ app/src/main/java/com/czm/xcdanmuview/MainActivity.java
@@ -0,0 +1,60 @@
+package com.czm.xcdanmuview;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private XCDanmuView mDanmuView;
+    private List<View> mViewList;
+    private String[] mStrItems = {
+            "搜狗","百度",
+            "腾讯","360",
+            "阿里巴巴","搜狐",
+            "网易","新浪",
+            "搜狗-上网从搜狗开始","百度一下,你就知道",
+            "必应搜索-有求必应","好搜-用好搜，特顺手",
+            "Android-谷歌","IOS-苹果",
+            "Windows-微软","Linux"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDanmuView();
+        initListener();
+    }
+
+    private void initListener() {
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mDanmuView.isWorking()) {
+                    mDanmuView.hide();
+                    ((Button) view).setText("开启弹幕");
+                } else {
+                    mDanmuView.start();
+                    ((Button) view).setText("关闭弹幕");
+                }
+            }
+        });
+    }
+
+    private void initDanmuView() {
+        mDanmuView = (XCDanmuView)findViewById(R.id.danmu);
+        mDanmuView.initDanmuItemViews(mStrItems);
+    }
+
+}
