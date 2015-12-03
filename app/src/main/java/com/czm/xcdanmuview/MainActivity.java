b7e2feb14afdceb4bbf931b30963d3f67e0ce3cd
Jamy Cai <jczmdeveloper@gmail.com>
2015-12-03 15:31:06 +0800

    bug fix
    bug fix

--- app/src/main/java/com/czm/xcdanmuview/MainActivity.java
+++ app/src/main/java/com/czm/xcdanmuview/MainActivity.java
@@ -1,17 +1,9 @@
 package com.czm.xcdanmuview;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import java.util.List;
 
